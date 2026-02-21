081fe489a50198045612cbdf43e496294a554538
Chih-Yuan Hsu <53067662+cyhsuTN@users.noreply.github.com>
2026-02-20 19:11:56 -0600

    Update paCalculation.R

--- R/paCalculation.R
+++ R/paCalculation.R
@@ -52,11 +52,13 @@ paCalculation <- function(N, s, pp=9/11, ps=0.75, beta=1) {
     pa <- 0
     details <- NULL
   } else if(test1(0) < Fb) {
-    print(paste0("Warning: no value of pa satisfies Fb with ", "pp=", round(pp,2), " and ps=", round(ps,2), ". Fb must be less than ", round(test1(0), 2),
+    print(paste0("Warning: no value of pa satisfies Fb with ", "pp=", round(pp,2), " and ps=", round(ps,2),
+                 #". Fb cannot be larger than ", round(test1(0), 2),
                  ". Force pa = 0 so it aligns more closely with pp."))
     pa <- 0; details <- NULL
   } else if(test1(1) > Fb) {
-    print(paste0("Warning: no value of pa satisfies Fb with ", "pp=", round(pp,2), " and ps=", round(ps,2), ". Fb must be larger than ", round(test1(1), 2),
+    print(paste0("Warning: no value of pa satisfies Fb with ", "pp=", round(pp,2), " and ps=", round(ps,2),
+                 #". Fb cannot be less than ", round(test1(1), 2),
                  ". Force pa = 1 so it aligns more closely with pp."))
     pa <- 1; details <- NULL
   } else {
